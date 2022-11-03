a9787013283dec0f9225ff725d2d4bd088838cc2
TOHU <tomozou.huang@pwc.com>
2022-11-03 16:09:52 +0800

    update 20221103 16:09

--- EzTool.SDK.WPF.ControlKit.DEMO.NET6/DEMO/EzWindow/EzWindowViewModel.cs
+++ EzTool.SDK.WPF.ControlKit.DEMO.NET6/DEMO/EzWindow/EzWindowViewModel.cs
@@ -1,4 +1,12 @@
-﻿using EzTool.SDK.WPF.Unicorn.HumbleObjects;
+﻿using EzTool.SDK.Router.Core.Interface;
+using EzTool.SDK.WPF.ControlKit.DEMO.EzWindow.Events;
+using EzTool.SDK.WPF.Mask;
+using EzTool.SDK.WPF.Mask.Core.Interfaces;
+using EzTool.SDK.WPF.Region.Core.Interfaces;
+using EzTool.SDK.WPF.Unicorn.BaseObjects;
+using EzTool.SDK.WPF.Unicorn.HumbleObjects;
+using EzTool.SDK.WPF.Unicorn.Utilities;
+using EzTool.SDK.WPF.Unicorn.Views.Master;
 using EzTool.SDK.WPF.ViewActions.Interface;
 
 using System;
@@ -6,25 +14,108 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace EzTool.SDK.WPF.ControlKit.DEMO.EzWindow
 {
-    public class EzWindowViewModel : IWindowView
+    public class EzWindowViewModel : IHostView, IWindowView, IDialogView
     {
+        #region -- 變數宣告 ( Declarations ) --   
+
+        private Func<bool> l_objIsWaiting = new Func<bool>(() => { return false; });
+
+        #endregion
+
+        #region -- 屬性 ( Properties ) --
+
+        public EzWindowView ViewWindow { get; set; }
+        private MaskLayer ViewMask { get; set; }
+        private IRegionHost RegionHost { get; set; }
+
+        #endregion
+
         public IPresenter Presenter { get; set; }
         public string DTO { get; set; }
 
         public void ExitView()
         {
-            
+
         }
+
 
         public void ShowDialog()
         {
+            var objDataContext = new EzWindowViewContext()
+            {
+                Presenter = Presenter,
+                HashCode = GetHashCode().ToString(),
+                IsShowProgress = false 
+            };
+            var objMsgEvent = new EzWindowViewFunctionBarNotifyMsgEvent() { ViewContext = objDataContext };
+
+            objDataContext.RelayCommand.Register(objMsgEvent);
+
             new DispatcherProxy().Invoke(() =>
             {
-                new EzWindowView().ShowDialog();
+                var objViewControl = ViewControlBuilder.Build<EzWindowMainView>(objDataContext);
+
+                ViewMask = MaskLayer.Initial(objDataContext);
+                ViewMask.Mount(objViewControl);
+                ViewWindow = new EzWindowView() { DataContext = objDataContext };
+                ViewWindow.AppGrid.Children.Add((UIElement)ViewMask.Container);
+                ViewWindow.ShowDialog();
             });
         }
+
+        public void Embed(IClientView pi_objView)
+        {
+            throw new NotImplementedException();
+        }
+        public bool IsHostOf(IRequire pi_objRequire)
+        {
+            return true;
+        }
+
+        public IDialogView Popup(IClientView pi_objView)
+        {
+            if (pi_objView is IMaskDialogView objDialogView)
+            {
+                var bIsWait = true;
+                var objOnDialogExit = default(Action);
+
+                objOnDialogExit = new Action(() =>
+                {
+                    new DispatcherProxy().Invoke(() =>
+                    {
+                        bIsWait = false;
+                        objDialogView.ViewExited -= objOnDialogExit;
+                    });
+                });
+
+                l_objIsWaiting = new Func<bool>(() => { return bIsWait; });
+                objDialogView.ViewExited += objOnDialogExit;
+                ViewMask.Popup(objDialogView);
+            }
+
+            return this;
+        }
+
+        #region -- 介面實做 ( Implements ) - [IDialogView] --
+
+        public virtual void Waitting()
+        {
+            while (l_objIsWaiting.Invoke())
+            {
+                System.Threading.Thread.Sleep(10);
+            }
+        }
+
+        #endregion
+    }
+
+    public class EzWindowViewContext : BaseViewContext
+    {
+        public string HashCode { get; set; }
     }
 }
--- EzTool.SDK.WPF.ControlKit.NET6/ControlKit/WindowKit/EzWindow.cs
+++ EzTool.SDK.WPF.ControlKit.NET6/ControlKit/WindowKit/EzWindow.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using EzTool.SDK.WPF.ControlKit.FunctionBarKit;
+
+using System.Windows;
 
 namespace EzTool.SDK.WPF.ControlKit.WindowKit
 {
@@ -13,18 +15,43 @@ namespace EzTool.SDK.WPF.ControlKit.WindowKit
 
         #endregion
 
+        #region -- 事件處理 ( Event Handlers ) --
+
+        /// <summary>
+        /// FunctionBar 属性更改时调用此方法。
+        /// </summary>
+        /// <param name="oldValue">FunctionBar 属性的旧值。</param>
+        /// <param name="newValue">FunctionBar 属性的新值。</param>
+        protected virtual void OnFunctionBarChanged(WindowFunctionBar oldValue, WindowFunctionBar newValue) { }
+
+        private static void OnFunctionBarChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            var oldValue = (WindowFunctionBar)args.OldValue;
+            var newValue = (WindowFunctionBar)args.NewValue;
+
+            if (oldValue != newValue && obj is EzWindow objWindow)
+            {
+                objWindow?.OnFunctionBarChanged(oldValue, newValue);
+            }
+        }
+
+        #endregion
+
         #region -- 屬性 ( Properties ) --
 
         #region FunctionBar
 
-        //public WindowFunctionBar FunctionBar
-        //{
-        //    get => (WindowFunctionBar)GetValue(FunctionBarProperty);
-        //    set => SetValue(FunctionBarProperty, value);
-        //}
+        public WindowFunctionBar FunctionBar
+        {
+            get => (WindowFunctionBar)GetValue(FunctionBarProperty);
+            set => SetValue(FunctionBarProperty, value);
+        }
 
-        //public static readonly DependencyProperty FunctionBarProperty =
-        //  DependencyProperty.Register(nameof(FunctionBar), typeof(WindowFunctionBar), typeof(EzWindow), new PropertyMetadata(default(WindowFunctionBar), OnFunctionBarChanged));
+        public static readonly DependencyProperty FunctionBarProperty =
+            DependencyProperty.Register(
+                nameof(FunctionBar),
+                typeof(WindowFunctionBar),
+                typeof(EzWindow), new PropertyMetadata(default(WindowFunctionBar), OnFunctionBarChanged));
 
         #endregion
 
@@ -40,10 +67,10 @@ namespace EzTool.SDK.WPF.ControlKit.WindowKit
 
         private static readonly DependencyPropertyKey IsNonClientActivePropertyKey =
             DependencyProperty.RegisterReadOnly(
-                "IsNonClientActive", typeof(bool), 
+                "IsNonClientActive", typeof(bool),
                 typeof(EzWindow), new FrameworkPropertyMetadata(false));
 
-        public static readonly DependencyProperty IsNonClientActiveProperty = 
+        public static readonly DependencyProperty IsNonClientActiveProperty =
             IsNonClientActivePropertyKey.DependencyProperty;
 
         #endregion
