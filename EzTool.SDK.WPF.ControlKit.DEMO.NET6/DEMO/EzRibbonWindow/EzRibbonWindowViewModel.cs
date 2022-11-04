7f5a734e7b2fbdf60633dd883e62b025038fc3e3
TOHU <tomozou.huang@pwc.com>
2022-11-04 17:10:20 +0800

    update 20221104 17:10

--- EzTool.SDK.WPF.ControlKit.DEMO.NET6/DEMO/EzRibbonWindow/EzRibbonWindowViewModel.cs
+++ EzTool.SDK.WPF.ControlKit.DEMO.NET6/DEMO/EzRibbonWindow/EzRibbonWindowViewModel.cs
@@ -1,4 +1,12 @@
-﻿using EzTool.SDK.WPF.Mask.HumbleObjects;
+﻿using EzTool.SDK.Router.Core.Interface;
+using EzTool.SDK.WPF.ControlKit.DEMO.EzRibbonWindow.Events;
+using EzTool.SDK.WPF.ControlKit.DEMO.EzWindow;
+using EzTool.SDK.WPF.Mask;
+using EzTool.SDK.WPF.Mask.Core.Interfaces;
+using EzTool.SDK.WPF.Mask.HumbleObjects;
+using EzTool.SDK.WPF.Region.Core.Interfaces;
+using EzTool.SDK.WPF.Unicorn.BaseObjects;
+using EzTool.SDK.WPF.Unicorn.Utilities;
 using EzTool.SDK.WPF.ViewActions.Interface;
 
 using System;
@@ -6,31 +14,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace EzTool.SDK.WPF.ControlKit.DEMO.EzRibbonWindow
 {
-    public class EzRibbonWindowViewModel : IWindowView
+    public class EzRibbonWindowViewModel : IWindowView, IHostView, IDialogView
     {
 
         #region -- 變數宣告 ( Declarations ) --   
-        #endregion
 
-        #region -- 建構/解構 ( Constructors/Destructor ) --
-        #endregion
+        private Func<bool> l_objIsWaiting = new Func<bool>(() => { return false; });
 
-        #region -- 事件處理 ( Event Handlers ) --
-        #endregion
-
-        #region -- 靜態方法 (Shared Method ) --
-        #endregion
-
-        #region -- 方法 ( Public Method ) --
         #endregion
 
         #region -- 屬性 ( Properties ) --
-        #endregion
 
-        #region -- 事件 ( Events ) --
+        public EzRibbonWindowView ViewWindow { get; set; }
+        private MaskLayer ViewMask { get; set; }
+        private IRegionHost RegionHost { get; set; }
+
         #endregion
 
         #region -- 介面實做 ( Implements ) - [IWindowView] --
@@ -40,32 +42,91 @@ namespace EzTool.SDK.WPF.ControlKit.DEMO.EzRibbonWindow
 
         public void ExitView()
         {
-            throw new NotImplementedException();
+
         }
 
         public void ShowDialog()
         {
+            var objDataContext = new EzRibbonWindowViewContext()
+            {
+                Presenter = Presenter,
+                HashCode = GetHashCode().ToString(),
+                IsShowProgress = false
+            };
+            var objCopyEvent = new EzRibbonWindowCopyEven() { ViewContext = objDataContext };
+
+            objDataContext.RelayCommand.Register(objCopyEvent);
             new DispatcherProxy().Invoke(() =>
             {
-                new EzRibbonWindowView().ShowDialog();
+                var objViewControl = ViewControlBuilder.Build<EzRibbonWindowMainView>(objDataContext);
+
+                ViewMask = MaskLayer.Initial(objDataContext);
+                ViewMask.Mount(objViewControl);
+                ViewWindow = new EzRibbonWindowView() { DataContext = objDataContext };
+                ViewWindow.AppGrid.Children.Add((UIElement)ViewMask.Container);
+                ViewWindow.ShowDialog();
             });
         }
 
         #endregion
 
-        #region -- 介面實做 ( Implements ) - [InterfaceName] --
+        #region -- 介面實做 ( Implements ) - [IHostView] --
+
+        public bool IsHostOf(IRequire pi_objRequire)
+        {
+            return true;
+        }
+
+        public void Embed(IClientView pi_objView)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDialogView Popup(IClientView pi_objView)
+        {
+            if (pi_objView is IMaskDialogView objDialogView)
+            {
+                var bIsWait = true;
+                var objOnDialogExit = default(Action);
+
+                objOnDialogExit = new Action(() =>
+                {
+                    new DispatcherProxy().Invoke(() =>
+                    {
+                        bIsWait = false;
+                        objDialogView.ViewExited -= objOnDialogExit;
+                    });
+                });
+
+                l_objIsWaiting = new Func<bool>(() => { return bIsWait; });
+                objDialogView.ViewExited += objOnDialogExit;
+                ViewMask.Popup(objDialogView);
+            }
+
+            return this;
+        }
+
+
         #endregion
 
-        #region -- 介面實做 ( Implements ) - [InterfaceName] --
+        #region -- 介面實做 ( Implements ) - [IDialogView] --
+
+        public void Waitting()
+        {
+            while (l_objIsWaiting.Invoke())
+            {
+                System.Threading.Thread.Sleep(10);
+            }
+        }
+
         #endregion
 
-        #region -- 私有函式 ( Private Method) --
-        #endregion
+    }
 
-        #region -- 衍生函式 ( Protected Method ) -- 
-        #endregion
-
-        #region -- 抽象函式 ( Abstract Method ) --        
-        #endregion
+    public class EzRibbonWindowViewContext : BaseViewContext
+    {
+        public IPresenter Presenter { get; set; }
+        public string HashCode { get; set; }
+        public bool IsShowProgress { get; set; }
     }
 }
