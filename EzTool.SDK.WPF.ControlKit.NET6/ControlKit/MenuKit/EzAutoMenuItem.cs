484620f8d5bc2eeaa0ce2047a5dce4a8b4d70106
TOHU <tomozou.huang@pwc.com>
2022-11-18 16:46:26 +0800

    update 20221118 16:46

--- EzTool.SDK.WPF.ControlKit.NET6/ControlKit/MenuKit/EzAutoMenuItem.cs
+++ EzTool.SDK.WPF.ControlKit.NET6/ControlKit/MenuKit/EzAutoMenuItem.cs
@@ -1,12 +1,18 @@
-﻿using System.Windows.Controls;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Windows.Controls;
 
 namespace EzTool.SDK.WPF.ControlKit.MenuKit
 {
     public class EzAutoMenuItem : MenuItem
     {
+        #region -- 建構/解構 ( Constructors/Destructor ) --
+
         public EzAutoMenuItem()
         {
             DefaultStyleKey = typeof(EzAutoMenuItem);
         }
+
+        #endregion
+
     }
 }
---
+++ EzTool.SDK.WPF.ControlKit.NET6/ControlKit/MenuKit/MenuItemContainerTemplateSelector.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using System.IO;
+using System.Text;
+using System.Windows.Markup;
+
+namespace EzTool.SDK.WPF.ControlKit.MenuKit
+{
+    /// <summary>
+    /// https://stackoverflow.com/questions/31214027/how-to-correctly-bind-a-viewmodel-which-include-separators-to-wpfs-menu
+    /// </summary>
+    public class MenuItemContainerTemplateSelector : ItemContainerTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, ItemsControl parentItemsControl)
+        {
+            DataTemplate objReturn;
+            MemoryStream ms;
+
+            if (item == null)
+            {
+                ms = new MemoryStream(Encoding.UTF8.GetBytes(
+                   $@"<DataTemplate xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+                                                                 xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""                                                                             
+                                                                 xmlns:c=""clr-namespace:MyApp.Converters;assembly=MyApp"">
+                            <Separator />
+                          </DataTemplate>"));
+            }
+            else
+            {
+                ms = new MemoryStream(Encoding.UTF8.GetBytes(
+                    $@"<DataTemplate xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+                                                                 xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""                                                                             
+                                                                 xmlns:c=""clr-namespace:MyApp.Converters;assembly=MyApp"">
+                            <MenuItem Header=""{{Binding Header}}"" />
+                          </DataTemplate>"));
+
+            }
+            objReturn = (DataTemplate)XamlReader.Load(ms);
+
+            return objReturn;
+        }
+    }
+}
