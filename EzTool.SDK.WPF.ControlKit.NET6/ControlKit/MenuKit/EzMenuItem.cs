4ece18b4bdf935e2e4bcf6735a0a5a0e03a8aed9
TOHU <tomozou.huang@pwc.com>
2022-11-18 10:37:53 +0800

    update 20221118 10:37

---
+++ EzTool.SDK.WPF.ControlKit.NET6/ControlKit/MenuKit/EzMenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace EzTool.SDK.WPF.ControlKit.MenuKit
+{
+
+    public class EzMenuItem : MenuItem
+    {
+        public  EzMenuItem()
+        {
+            DefaultStyleKey = typeof(EzMenuItem);
+        }
+    }
+}
